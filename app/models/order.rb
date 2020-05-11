37b79aa0d69252e60403679a957e848a69f86fd9
Kieran Scannell <kierscan@gmail.com>
2020-05-11 09:29:19 -0500

    Install rubocop and reformat based on suggestions

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: orders
@@ -29,32 +31,40 @@
 #  payment_method                              :string
 #
 class Order < ApplicationRecord
-  validates :name, :phone, :email, :service_type, :service_date, 
-    :payment_method, presence: true
+  validates :name, :phone, :email, :service_type, :service_date,
+            :payment_method, presence: true
   validates :substitutions, inclusion: { in: [true, false] }
   validate :service_date_cannot_be_in_the_past, :service_date_cannot_be_sunday
-  validates :service_type, inclusion: { in: ["Curbside Pick Up", "Delivery"] }
+  validates :service_type, inclusion: { in: ['Curbside Pick Up', 'Delivery'] }
   validate :desired_pick_up_time_with_operating_hours
-  validates :payment_method, inclusion: { in: ["Debit or Credit Card",
-    "In-House Account", "Check", "Cash", "WIC", "SNAP"] }
-
+  validates :payment_method, inclusion: { in: [
+    'Debit or Credit Card',
+    'In-House Account',
+    'Check',
+    'Cash',
+    'WIC',
+    'SNAP'
+  ] }
+  
   private
 
   def service_date_cannot_be_in_the_past
-    if self.service_date.present? && self.service_date < Date.today
-      errors.add(:service_date, "can't be in the past")
-    end
+    return unless service_date.present? && service_date < Time.zone.today
+
+    errors.add(:service_date, "can't be in the past")
   end
 
   def service_date_cannot_be_sunday
-    if self.service_date.sunday?
-      errors.add(:service_date, "must be Monday through Saturday")
-    end
+    return unless service_date.sunday?
+
+    errors.add(:service_date, 'must be Monday through Saturday')
   end
 
   def desired_pick_up_time_with_operating_hours
-    if self.desired_pick_up_time.hour < 8 || self.desired_pick_up_time.hour > 17
-      errors.add(:desired_pick_up_time, "must be between 8 am and 6 pm")
+    unless desired_pick_up_time.hour < 8 || desired_pick_up_time.hour > 17
+      return
     end
+
+    errors.add(:desired_pick_up_time, 'must be between 8 am and 6 pm')
   end
 end
